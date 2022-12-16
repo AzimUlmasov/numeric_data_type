05b2ebbc3a879929fd5ef3388c09d9955bcc1ac7
AzimUlmasov <60934790+AzimUlmasov@users.noreply.github.com>
2022-12-16 17:25:42 +0500

    Update reversed_num.py

--- reversed_num.py
+++ reversed_num.py
@@ -1,11 +1,21 @@
 # Find the given number by swapping its digits
-number = int(input("Enter the integer number: "))
+# number = int(input("Enter the integer number: "))
 
-revs_number = 0
+# revs_number = 0
 
-while (number > 0):
+# while (number > 0):
 
-    remainder = number % 10
-    revs_number = (revs_number * 10) + remainder 
-    number = number // 10 
-print("The reverse number is: {}".format(revs_number))
+#     remainder = number % 10
+#     revs_number = (revs_number * 10) + remainder 
+#     number = number // 10 
+# print("The reverse number is: {}".format(revs_number))
+
+# Find the given number by swapping its digits
+x = 2561 
+x1 = x // 1000 #2
+x2 = x // 100 % 10 #5
+x3 = x % 100 // 10 #6
+x4 = x % 10 #1
+
+x = x4 * 1000 + x3 * 100 + x2 * 10 + x1
+print(x)
